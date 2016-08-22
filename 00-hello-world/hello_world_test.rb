c17df6889b29b5afe5241fdd917462568f7a6d5f
Dianne Laguerta <diannelaguerta@gmail.com>
2016-08-22 16:25:25 -0700

    solving hello_world

--- 00-hello-world/hello_world_test.rb
+++ 00-hello-world/hello_world_test.rb
@@ -17,12 +17,14 @@ class HelloWorldTest < Minitest::Test
   end
 
   def test_other_sample_name
+
     assert_equal 'Hello, Bob!', hello_world('Bob'), <<-MSG
     When given "Bob" it should greet Bob!
     MSG
   end
 
   def test_empty_string_name
+  
     assert_equal 'Hello, World!', hello_world(''), <<-MSG
     When given an empty string it should greet the world!
     MSG
