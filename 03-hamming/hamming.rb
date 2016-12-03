85fab7aea6ed47450d0ab02cb94252ded1c51a7f
Dianne Laguerta <diannelaguerta@gmail.com>
2016-12-03 13:31:14 -0800

    original answer to hamming

---
+++ 03-hamming/hamming.rb
@@ -0,0 +1,21 @@
+# class Hamming
+#   def self.compute(a, b)
+#     if a.length != b.length
+#       raise ArgumentError
+#     end
+#     if a == b
+#       return 0
+#     else
+#
+#       distance = 0
+#       a.length.times do |i|
+#         if a[i] != b[i]
+#           distance += 1
+#         end
+#
+#       end
+#         return distance
+#     end
+#
+#   end
+# end
